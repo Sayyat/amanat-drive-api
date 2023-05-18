83416ecfad604d945dbdbf6afdee8abf2d85e3da
Syzdykov Sultan <sultansyzdyc@gmail.com>
2023-05-18 16:05:04 +0600

    fix: added adaptive style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
-import { click } from '../../store/slices/burgerSlice';
+import LinesEllipsis from "react-lines-ellipsis";
+
+import logo from "../../assets/images/logo-dark.png";
+import { click } from "../../store/slices/burgerSlice";
 import logout from "../../assets/images/logout.png";
 import styles from "./header.module.scss";
 
@@ -10,20 +13,53 @@ const Header = () => {
   const dispatch = useDispatch();
 
   const handleBurger = () => {
-    dispatch(click())
-  }
+    dispatch(click());
+  };
 
   return (
     <header className={styles.header}>
       <div className={styles.headerRight}>
         <div className={styles.burger} onClick={handleBurger}>
-          <svg width="24" height="24" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.5 6H10.5" stroke="black" strokeWidth="0.75" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M1.5 3H10.5" stroke="black" strokeWidth="0.75" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M1.5 9H10.5" stroke="black" strokeWidth="0.75" strokeLinecap="round" strokeLinejoin="round"/>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.5 6H10.5"
+              stroke="black"
+              strokeWidth="0.75"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M1.5 3H10.5"
+              stroke="black"
+              strokeWidth="0.75"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M1.5 9H10.5"
+              stroke="black"
+              strokeWidth="0.75"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
           </svg>
         </div>
-        <h2 className={styles.pageTitle}>Пайщики автомобилей</h2>
+
+        <h2 className={styles.pageTitle}>
+          <LinesEllipsis
+            text="Пайщики автомобилей"
+            maxLine="2"
+            ellipsis="..."
+            trimRight
+            basedOn="letters"
+          />
+        </h2>
       </div>
       <div className={styles.logout}>
         <span>Выйти</span>
--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -81,33 +81,35 @@ const Table = ({ data, iin }) => {
     // <TableAntd columns={columns} dataSource={data} />
 
     <div className={styles.table}>
-      {data.length <= 0 ? (
-        <div className={styles.empty}>
-          <Image src={empty} alt="Empty" />
-          <div className={styles.empty__title}>Нет данных</div>
-        </div>
-      ) : (
-        data.map((table, index) => (
-          <table key={index}>
-            {table.map((row, rowIndex) => (
-              <tr
-                key={rowIndex}
-                className={row["longIin"] === iin ? styles.me : ""}
-              >
-                <td>{row["index"]}</td>
-                <td>{row["contractNumber"]}</td>
-                <td>{row["date"]}</td>
-                <td>{row["fullname"]}</td>
-                <td>
-                  {row["longIin"] === iin
-                    ? row["longIin"]
-                    : row["shortIin"].padStart(12, "#")}
-                </td>
-              </tr>
-            ))}
-          </table>
-        ))
-      )}
+      <div className={styles.tableWrapper}>
+        {data.length <= 0 ? (
+          <div className={styles.empty}>
+            <Image src={empty} alt="Empty" />
+            <div className={styles.empty__title}>Нет данных</div>
+          </div>
+        ) : (
+          data.map((table, index) => (
+            <table key={index}>
+              {table.map((row, rowIndex) => (
+                <tr
+                  key={rowIndex}
+                  className={row["longIin"] === iin ? styles.me : ""}
+                >
+                  <td>{row["index"]}</td>
+                  <td>{row["contractNumber"]}</td>
+                  <td>{row["date"]}</td>
+                  <td>{row["fullname"]}</td>
+                  <td>
+                    {row["longIin"] === iin
+                      ? row["longIin"]
+                      : row["shortIin"].padStart(12, "#")}
+                  </td>
+                </tr>
+              ))}
+            </table>
+          ))
+        )}
+      </div>
     </div>
   );
 };
