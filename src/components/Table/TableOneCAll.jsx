c0258326dbec6427e31056dc04c3dd7cfbb50c26
Sayyat <sayatraykulov@gmail.com>
2023-05-31 17:37:34 +0600

    paid count aligned to right

--- src/components/Table/TableOneCAll.jsx
+++ src/components/Table/TableOneCAll.jsx
@@ -12,51 +12,58 @@ const TableOneCAll = ({data, type, pathIndex}) => {
     return (
         <div className={`${styles.table} ${styles.tableFlex}`}>
 
-                {data.sharers.length <= 0 ? (
-                    <div className={styles.empty}>
-                        <Image src={empty} alt="Empty"/>
-                        <div className={styles.empty__title}>Нет данных</div>
-                    </div>
-                ) : (
-                    <>
-                        <h3 className={styles.tableTitle}>{data?.listName}</h3>
-                        <div className={styles.tableWrapper}>
-                            <table className={styles.oneCAllTable}>
-                                <thead>
-                                <tr>
-                                    <td>№</td>
-                                    <td>Ф.И.О.</td>
-                                    <td>ИИН</td>
-                                    <td>Договор</td>
-                                    <td>Пополнено (<span className={styles.OneCLink}>{data.paidCount} / {data.sharers.length}</span>)</td>
+            {data.sharers.length <= 0 ? (
+                <div className={styles.empty}>
+                    <Image src={empty} alt="Empty"/>
+                    <div className={styles.empty__title}>Нет данных</div>
+                </div>
+            ) : (
+                <>
+                    <h3 className={styles.tableTitle}>{data?.listName}</h3>
+                    <div className={styles.tableWrapper}>
+                        <table className={styles.oneCAllTable}>
+                            <thead>
+                            <tr>
+                                <td>№</td>
+                                <td>Ф.И.О.</td>
+                                <td>ИИН</td>
+                                <td>Договор</td>
+                                <td>Пополнено <br/>
+                                    <span className={styles.green}>{data.paidCount}</span>&nbsp;/&nbsp;
+                                    <span className={styles.red}>{data.sharers.length}</span>
+                                </td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {data.sharers.map((row, rowIndex) => (
+                                <tr key={rowIndex}>
+                                    <td>{rowIndex + 1}</td>
+                                    <td>{row["fullname"]}</td>
+                                    <td className={styles.OneCTdLink}><
+                                        Link className={styles.red} href={{
+                                        pathname: `/${pathIndex}/${row["iin"] ? row["iin"] : row["fullname"]}`,
+                                        query: isType
+                                    }}>
+                                        {row["iin"] ? row["iin"] : "Подробнее"}
+                                    </Link>
+                                    </td>
+                                    <td>{row["contractNumber"]}</td>
+                                    <td style={{textAlign: "right"}}>{row["isPaid"] ? <Image src={truePaid} alt="true check"/> :
+                                        <Image src={falsePaid} alt="false check"/>}</td>
                                 </tr>
-                                </thead>
-                                <tbody>
-                                {data.sharers.map((row, rowIndex) => (
-                                    <tr key={rowIndex}>
-                                        <td>{rowIndex + 1}</td>
-                                        <td>{row["fullname"]}</td>
-                                        <td className={styles.OneCTdLink}><
-                                            Link className={styles.OneCLink} href={{ pathname: `/${pathIndex}/${row["iin"] ? row["iin"] : row["fullname"]}`, query: isType }}>
-                                            {row["iin"] ? row["iin"] : "Подробнее"}
-                                        </Link>
-                                        </td>
-                                        <td>{row["contractNumber"]}</td>
-                                        <td>{row["isPaid"] ? <Image src={truePaid} alt="true check" /> : <Image src={falsePaid} alt="false check"/>}</td>
-                                    </tr>
-                                ))}
-                                {/*<tr key={"summaryRow"}>*/}
-                                {/*    <td>{data.summary["buildingPrice"]}</td>*/}
-                                {/*    <td>{data.summary["contractNumber"]}</td>*/}
-                                {/*    <td>{data.summary["date"]}</td>*/}
-                                {/*    <td>{data.summary["fullname"]}</td>*/}
-                                {/*    <td>{data.summary["iin"]}</td>*/}
-                                {/*</tr>*/}
-                                </tbody>
-                            </table>
-                        </div>
-                    </>
-                )}
+                            ))}
+                            {/*<tr key={"summaryRow"}>*/}
+                            {/*    <td>{data.summary["buildingPrice"]}</td>*/}
+                            {/*    <td>{data.summary["contractNumber"]}</td>*/}
+                            {/*    <td>{data.summary["date"]}</td>*/}
+                            {/*    <td>{data.summary["fullname"]}</td>*/}
+                            {/*    <td>{data.summary["iin"]}</td>*/}
+                            {/*</tr>*/}
+                            </tbody>
+                        </table>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
