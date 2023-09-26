76eccba1e975849a437ce6ce38d8d2ad28f42c1b
sayat <sayatraykulov@gmail.com>
2023-09-26 16:56:42 +0600

    Revert "update: google oauth removed"
    This reverts commit 6aa7fd4e

---
+++ src/pages/api/auth/login.js
@@ -0,0 +1,10 @@
+import {login} from "@/backend/database/auth";
+
+export default async function (req, res) {
+    const {email} = JSON.parse(req.body)
+
+    const result = await login("", email);
+    res.status(200).json({
+        id: result.length === 0 ? null : result[0]?.id
+    })
+}
